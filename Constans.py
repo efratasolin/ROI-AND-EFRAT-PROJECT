602efc571af17833754bf78915aa508636a8ac71
efrat <efratas2014@gmail.com>
2022-04-13 12:25:47 +0300

    constans

---
+++ Constans.py
@@ -0,0 +1,22 @@
+player_x = 135
+player_y = 250
+obstacle_y = 0
+
+LANE1_X = 0
+LANE2_X = 165
+LANE3_X = 400
+LANE_DISTANCE = 200
+
+# dimensions of the object
+width = 230
+height = 230
+
+Obstacle_in_width = 75
+Obstacle_in_height = 75
+
+TRAIN_LENGTH = 150
+TRAIN_HEIGHT = 100
+
+# velocity / speed of movement
+vel = 2.5
+vel2 = 25
